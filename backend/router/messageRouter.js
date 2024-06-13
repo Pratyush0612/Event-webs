5cd799409c397dd667196601c2c0d33adc38947b
Pratyush Tyagi <86615560+Pratyush0612@users.noreply.github.com>
2024-06-13 11:22:49 +0530

    Delete backend directory

--- backend/router/messageRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendMessage } from "../controller/messageController.js";
-
-const router = express.Router();
-
-router.post("/send", sendMessage);
-
-export default router;
--- backend/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening at port ${process.env.PORT}`);
-  });
